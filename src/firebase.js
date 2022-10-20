09f5abc4123f1a619fd7c8564b9e15515946aadd
jakapozun <jaka.pozun@skaza.si>
2022-10-20 21:13:15 +0200

    added auth + deploy to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { getAuth } from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCvbN4xXXXEn6aaTDcHUqMeoAM_DsmiBko",
     authDomain: "netflix-react-2ac82.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
     messagingSenderId: "503295173035",
     appId: "1:503295173035:web:3bcf5f4d6eddc91083b2ee"
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
+const auth = getAuth(firebaseApp);
+
+export {auth, db}
